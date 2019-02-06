3dc37d7de97f44e9f6742caaf46e27b19c087107
Mineo Okuda <okuda@willstyle.co.jp>
2019-02-06 18:48:46 +0900

    update

--- dist/js/photoswipe-simplify.js
+++ dist/js/photoswipe-simplify.js
@@ -1,7 +1,7 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 /*!
- * photoswipe-simplify v0.0.2: PhotoSwipe.js simplify by the VanillaJS.
+ * photoswipe-simplify v0.0.3: PhotoSwipe.js simplify by the VanillaJS.
  * (c) 2019 Mineo Okuda
  * MIT License
  * git+ssh://git@github.com:min30327/photoswipe-simplify.git
